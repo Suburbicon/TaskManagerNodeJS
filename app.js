12bb0ff345f2c1825f63cc20beae6d1fa3932f44
d.khasenov <khassenov22@gmail.com>
2023-03-24 21:56:25 +0600

    provide task controller and crud db

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+
+// middleware
+
+app.use(express.json())
 
 app.get('/hello', (req, res) => {
 	res.send('Task Manager App')
 })
 
+app.use('/api/v1/tasks', tasks)
+
 const port = 3000
 
-app.listen(port, () => console.log(`Server is listening on ${port} 🥑🥑🥑`))
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI)
+			.then(() => console.log('CONNECTED TO THE DB 💥💥💥'))
+		app.listen(port, () => console.log(`Server is listening on ${port} 🥑🥑🥑`))
+	} catch (error) {
+		console.log(error + ' ❗️❗️❗️')
+	}
+}
+
+start()
